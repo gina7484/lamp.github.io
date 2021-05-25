ffaa51603bcf5993b1a47c9ee9e1be7d71b245f5
gina7484 <gina7484@naver.com>
2021-05-26 03:29:47 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# lamp.github.io
-[2021_1] HCI project LAMP (Learn Animated ML models with Papers)
+# hci_project
---
+++ vgg.js
@@ -0,0 +1,62 @@
+
+// interactive panel - conv
+
+d3.select('#conv-input-candidate')
+    .selectAll('img')
+    .on('click', function () {
+        let targetCanvas = document.getElementById("conv-input");
+        let ctx = targetCanvas.getContext("2d");
+        // clear convas
+        ctx.clearRect(0, 0, targetCanvas.width, targetCanvas.height);
+
+        let img = this;
+        ctx.drawImage(img, 0, 0, targetCanvas.clientWidth, targetCanvas.clientHeight);
+    });
+
+d3.select('#conv-filter-candidate')
+    .selectAll('img')
+    .on('click', function () {
+        let targetCanvas = document.getElementById("conv-filter");
+        let ctx = targetCanvas.getContext("2d");
+        // clear convas
+        ctx.clearRect(0, 0, targetCanvas.width, targetCanvas.height);
+
+        let img = this;
+        ctx.drawImage(img, 10, 10, targetCanvas.clientWidth - 20, targetCanvas.clientHeight - 20);
+    });
+
+d3.select('#conv-arrow')
+    .on('click', function () {
+        /*
+        let imgSpot = document.getElementById("conv-output-img");
+        imgSpot.src == './image_archive/edge_output/building_out.jpg';
+        */
+    });
+
+d3.select('#conv-input')
+    .on('click', function (event) {
+        console.log(event);
+        let clickedX = event.offsetX;
+        let clickedY = event.offsetY;
+
+        let context = this.getContext("2d");
+        let myImageData = context.getImageData(clickedX, clickedY, 3, 3).data;
+        let pixels = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                pixels[i][j] = myImageData[4 * (i * 3 + j)];
+            }
+        }
+
+
+        d3.selectAll('#grid')
+            .selectAll('.square')
+            .each(function (d, i) {
+                let row = Math.floor(i / 3);
+                let col = i % 3;
+                let grayscale = pixels[row][col];
+                this.style.fill = `rgb(${grayscale},${grayscale},${grayscale})`;
+            });
+
+
+    });
